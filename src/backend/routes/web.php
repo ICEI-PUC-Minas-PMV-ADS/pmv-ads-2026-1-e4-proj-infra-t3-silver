ae3c52c31cd1d0319bcd47f06dfeac608d4dbb2a
viniciusspl <viniciussoarespires@gmail.com>
2026-05-10 19:18:20 -0300

    Cria vies do CRUD de metas no backend Laravel

--- src/backend/routes/web.php
+++ src/backend/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\GoalViewController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::resource('goals', GoalViewController::class);
 });
 
 require __DIR__.'/settings.php';
