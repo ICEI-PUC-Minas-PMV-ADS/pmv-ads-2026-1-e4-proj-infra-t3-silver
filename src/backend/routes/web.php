cee8348a61b137364321dd3a74891fb5dd8f3485
Victor Silva <110117696+victordsilva1@users.noreply.github.com>
2026-05-10 19:11:36 -0300

    Add transactions rec e dep route to web.php

--- src/backend/routes/web.php
+++ src/backend/routes/web.php
@@ -7,6 +7,7 @@ Route::view('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
     Route::livewire('budgets', 'pages::budgets')->name('budgets.index');
+    Route::livewire('transactions', 'pages::transactions')->name('transactions.index');
 });
 
 require __DIR__.'/settings.php';
